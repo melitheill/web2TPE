47c042f9712e833631fa72ccc962af9aac5f2af0
Lucia Segovia <lusegovia01@gmail.com>
2023-10-12 14:39:48 -0300

    agregue carpetas y archivos, y le designe un nombre a la app

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav class="barraprincipal">
+          <img src="" alt="" href="" class="logo"> 
+          <div class="menu">
+            <ul>
+             <li><a href="formulario.html">Sing In</a></li>
+             </ul>
+          </div>
+          
+        </nav>
+       
+      </header>
+
+      <?php
+      // abro bloque php
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+      ?>
+      
+      
+      <footer>
+        <section class="cont-footer">
+          <div>
+            <ul>
+              <li>Sponsors</li>
+              <li>Prensa</li>
+           </ul>
+          </div>
+          <div >Ayuda
+            <ul>
+              <li>Contacto</li>
+              <li>Información</li>
+           </ul>
+          </div>
+          <div>Follow Us
+            <a href="https://api.instagram.com/" target="_blank"><i class="bii bi-instagram text-black"></i></a>
+            <a href="https://es-la.facebook.com/" target="_blank"><i class="bii bi-facebook text-black"></i></a>
+            <a href="https://twitter.com/?lang=es" target="_blank"><i class="bii bi-twitter text-black"></i></a>
+          </div>
+        </section> 
+      </footer>
+
+    
+</body>
+</html>
